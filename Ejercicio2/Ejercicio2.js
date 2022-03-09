5debda37a13e193e0b5d7f24bf5d6919d8a67c2d
Carlos Reyes <jc91ro@outlook.com>
2022-03-09 17:59:12 -0600

    first commit

---
+++ Ejercicio2/Ejercicio2.js
@@ -0,0 +1,95 @@
+## Cuestionario de Coercion
+#### Nombre: _______________________
+​
+1. Qué retona la siguiente expresión?
+```
+var sum = 3 + 2
+console.log(sum)
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+2. Qué retona la siguiente expresión?
+```
+typeof(3)
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+3. Qué retona la siguiente expresión?
+```
+typeof(3) === typeof("3")
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+4. Qué retona la siguiente expresión?
+```
+5 / 0
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+5. Qué retona la siguiente expresión?
+```
+3 / "bob"
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+6. Qué retona la siguiente expresión?
+```
+NaN === NaN
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+7. Qué retona la siguiente expresión?
+```
+typeof(NaN)
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+8. Cual es el valor de la siguiente expresion?
+```
+var hello
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+9. Qué retona la siguiente expresión?
+```
+"some" + " person"
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
+10. Qué retona la siguiente expresión?
+```
+var sport; 
+sport === undefined
+```
+​
+> _tu respuesta_
+​
+> _resultado en la consola_
+​
