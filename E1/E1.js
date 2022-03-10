a7e8ef965f0e39a9ca3a35584507cf7e8e27d820
Carlos Reyes <jc91ro@outlook.com>
2022-03-09 18:12:48 -0600

    Segundo commit

---
+++ E1/E1.js
@@ -0,0 +1,18 @@
+// 1
+var numero = Number(prompt('Escribe un numero'));
+var suma = numero * 2;
+console.log(suma)
+
+// 2
+var numero1 = Number(prompt("escribe un numero"));
+var numero2 = Number(prompt("escribe otro numero"));
+var resta = (numero1 - numero2);
+console.log(resta);
+
+//3
+var radio = Number(prompt("Ingresa el radio"));
+var area_circulo = 3.1416 * radio**2;
+console.log(area_circulo);
+
+//4
+console.log(8*6/2)
